1aaed9b258a5d632e4242e3ac8c2c6e0b3b467b2
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2022-08-05 21:51:56 +0100

    Selecting elements

--- talent.js
+++ talent.js
@@ -1,1 +1,6 @@
 
+const homeUnderline = document.querySelector('.home_underline');
+const impactUnderline = document.querySelector('.impact_underline');
+const whyUsUnderline = document.querySelector('.why_us_underline');
+    
+
