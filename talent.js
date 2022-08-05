63af5919522019cc476ac249bbe6c94359716699
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2022-08-05 21:52:30 +0100

    Working with JavaScript intersection observer API

--- talent.js
+++ talent.js
@@ -4,3 +4,18 @@ const impactUnderline = document.querySelector('.impact_underline');
 const whyUsUnderline = document.querySelector('.why_us_underline');
     
 
+
+// Using intersection observer API
+
+const homeSection = document.querySelector('.hero_section');
+const revealSection3 = function (entries, observer) {
+    const [entry] = entries;
+    // console.log(entry);
+
+    if(!entry.isIntersecting) return;
+  
+    impactUnderline.classList.remove('show');
+    homeUnderline.classList.add('show');
+    whyUsUnderline.classList.remove('show');
+
+}
